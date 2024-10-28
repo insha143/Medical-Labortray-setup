fd959c6a25b8f69ccf6cf9602b0a679d483f4fed
Insha <muhammad.inshaofficial@gmail.com>
2024-10-28 08:47:44 -0700

    updated links

--- src/Home.jsx
+++ src/Home.jsx
@@ -77,21 +77,6 @@ const Home = () => {
     );
 };
 
-// const Header = () => (
-//     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-//         <a className="navbar-brand" href="#">Admin Panel</a>
-//         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//             <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarNav">
-//             <ul className="navbar-nav ml-auto">
-//                 <li className="nav-item">
-//                     <a className="nav-link" href="#">Logout</a>
-//                 </li>
-//             </ul>
-//         </div>
-//     </nav>
-    
-// );
+
 
 export default Home;
